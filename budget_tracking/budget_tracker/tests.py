6c38ce9f784db85e83a46e4d2ab65038c2036103
Ezekiel John <johnezekiel526@gmail.com>
2025-12-10 06:08:42 -0800

    Unit test conducted

--- budget_tracking/budget_tracker/tests.py
+++ budget_tracking/budget_tracker/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from .models import Transaction
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+User = get_user_model()
+
+class TransactionAPITests(APITestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='u1', password='pass1234')
+        self.user2 = User.objects.create_user(username='u2', password='pass1234')
+
+def setUp(self):
+    self.user1 = User.objects.create_user(username='u1', password='pass1234')
+    self.user2 = User.objects.create_user(username='u2', password='pass1234')
+
+    # Create token for user1 to authenticate API requests
+    from rest_framework.authtoken.models import Token
+    self.token1 = Token.objects.create(user=self.user1)
+
+    # Authenticate test client using DRF TokenAuthentication
+    self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token1.key)
+
+    def test_create_transaction_requires_positive_amount(self):
+        self.client.login(username='u1', password='pass1234')
+        url = reverse('transactions-list')
+        response = self.client.post(url, {
+            "transaction_type": "EXPENSE",
+            "category": "FOOD",
+            "amount": "5.00",
+            "date": "2025-12-10"
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_cannot_view_other_users_transaction(self):
+        Transaction.objects.create(owner=self.user2, transaction_type='INCOME', category='SALARY', amount=100, date='2025-01-01')
+        self.client.login(username='u1', password='pass1234')
+        url = reverse('transactions-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # user1 should see zero transactions
+        self.assertEqual(len(response.json().get('results', response.json())), 0)
+
---
+++ budget_tracking/budget_tracking/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for budget_tracking project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from budget_tracker.views import TransactionViewSet, UserViewSet, ObtainAuthTokenAndUser
+from rest_framework.authtoken import views as drf_views
+from django.contrib import admin
+
+router = DefaultRouter()
+router.register(r'transactions', TransactionViewSet, basename='transactions')
+router.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/auth/login/', ObtainAuthTokenAndUser.as_view(), name='api-login'),
+    path('api/auth/token-login/', drf_views.obtain_auth_token, name='api-token-login')
+]
